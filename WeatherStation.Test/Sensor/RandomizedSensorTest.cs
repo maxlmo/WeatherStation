bf2d30e7881aa38b57ce6c9f4f0f589ce26c77d1
Max <max.moeller55@gmail.com>
2016-09-02 14:03:46 +0200

    Add test project and first test fixture.

---
+++ WeatherStation.Test/Sensor/RandomizedSensorTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using WeatherStation.Sensor;
+
+namespace WeatherStationTest.Sensor
+{
+    [TestFixture]
+    public class RandomizedSensorTest
+    {
+        [Test, Repeat(100)]
+        public void ReadBar_ReturnDouble_WithInRange()
+        {
+            var classUnderTest = new RandomizedSensor();
+
+            var result = classUnderTest.ReadBar();
+
+            const int minPressure = 980;
+            const int maxPressure = 1050;
+            Assert.That(result, Is.InRange(minPressure, maxPressure));
+        }
+
+        [Test, Repeat(100)]
+        public void ReadTemp_ReturnsDouble_WithInRange()
+        {
+            var classUnderTest = new RandomizedSensor();
+
+            var result = classUnderTest.ReadTemp();
+
+            const int minTemperature = -20;
+            const int maxTemperature = 40;
+            Assert.That(result, Is.InRange(minTemperature, maxTemperature));
+        }
+    }
+}
