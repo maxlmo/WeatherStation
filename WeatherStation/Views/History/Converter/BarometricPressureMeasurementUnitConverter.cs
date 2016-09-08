1156cd218e5cbcf98c88cc2656e4316de96e8a80
Max <max.moeller55@gmail.com>
2016-09-08 23:24:09 +0200

    REQ-10: Add converter for measurement units.

---
+++ WeatherStation/Views/History/Converter/BarometricPressureMeasurementUnitConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using WeatherStation.Properties;
+using WeatherStation.ViewModels.UnitSettings;
+
+namespace WeatherStation.Views.History.Converter
+{
+    public class BarometricPressureMeasurementUnitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var returnObject = (double)value;
+            var unit = (BarometricPressureUnit)Settings.Default.BarometricPressureUnit;
+            var multiplier = Constants.InHgMultiplier;
+
+            switch (unit)
+            {
+                case BarometricPressureUnit.InHg:
+                    return (returnObject * multiplier).ToString("F") + " inHg";
+
+                case BarometricPressureUnit.MBar:
+                    return returnObject.ToString("F") + " mBar";
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WeatherStation/Views/History/Converter/TemperatureMeasurementUnitConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using WeatherStation.Properties;
+using WeatherStation.ViewModels.UnitSettings;
+
+namespace WeatherStation.Views.History.Converter
+{
+    public class TemperatureMeasurementUnitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var temperatureValue = (double) value;
+            var unit = (TemperatureUnit) Settings.Default.TemperatureUnit;
+
+            switch (unit)
+            {
+                case TemperatureUnit.Celsius:
+                    return temperatureValue.ToString("F") + " °C";
+
+                case TemperatureUnit.Fahrenheit:
+                    return IntoFahrenheit(temperatureValue).ToString("F") + " °F";
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double IntoFahrenheit(double value)
+        {
+            return value*1.8 + 32;
+        }
+    }
+}
