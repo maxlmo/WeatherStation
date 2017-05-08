ff4e8fed20b4e509d040c1c4769c286746c108a7
Max <max.moeller55@gmail.com>
2017-05-08 13:35:50 +0200

    Implement validation rule for date string.

---
+++ WeatherStation.Test/Validation/StringToDateValidationRuleTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using WeatherStation.Validation;
+
+namespace WeatherStation.Test.Validation
+{
+    [TestFixture]
+    public class StringToDateValidationRuleTest
+    {
+        [Test]
+        public void Validate_ReturnsTrueOnValidDateString()
+        {
+            const string validTime = "12.05.2008";
+            var cut = new StringToDateValidationRule();
+
+            var result = cut.Validate(validTime, null);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Test]
+        public void Validate_ReturnsFalseOnInvalidDateString()
+        {
+            const string invalidTime = "#äö+üp";
+            var cut = new StringToDateValidationRule();
+
+            var result = cut.Validate(invalidTime, null);
+
+            result.IsValid.Should().BeFalse();
+        }
+    }
+}
