d1dd19d812594bcaecd0d53597365296d6253fad
Max <max.moeller55@gmail.com>
2017-05-15 20:54:31 +0200

    Add icon and style main window.

--- WeatherStation/Converter/EnumToImageConverter.cs
+++ WeatherStation/Converter/EnumToImageConverter.cs
@@ -9,15 +9,15 @@ namespace WeatherStation.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var trend = (BarometricPressureTrend) value;
+            var trend = (BarometricPressureTrend)value;
             switch (trend)
             {
                 case BarometricPressureTrend.Stable:
-                    return "Images/stable.jpg";
+                    return "../Images/stable.jpg";
                 case BarometricPressureTrend.Falling:
-                    return "Images/falling.jpg";
+                    return "../Images/falling.jpg";
                 case BarometricPressureTrend.Rising:
-                    return "Images/rising.jpg";
+                    return "../Images/rising.jpg";
             }
             return null;
         }
